23714d76c10ea1f7215bbcd9b52e3813203b1ea6
LeonDEvans <evansleon87@gmail.com>
2024-02-26 20:42:58 -0400

    completed exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num > 0)
+    num--
+    return  string.repeat(3)
+    
+    
 };
 
+repeatString("hey", 3)
+
 // Do not edit below this line
+
 module.exports = repeatString;
