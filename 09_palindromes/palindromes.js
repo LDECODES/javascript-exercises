17794135893395bd4ec116935c23091174c2ee89
LeonDEvans <evansleon87@gmail.com>
2024-05-25 11:57:14 -0400

    completed task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    var re = /[\W_]/g;
+    var lowRegStr = string.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;  
 };
 
 // Do not edit below this line
