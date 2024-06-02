ae9c82e4d5ce8017786165320eda0dd1966f4ed2
LeonDEvans <evansleon87@gmail.com>
2024-06-01 20:04:05 -0400

    task completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
-};
+    if (index < 0) {
+        return "OOPS"
+    }
+    var i;
+    var fib = [0, 1]; // Initialize array!
+  
+    for (i = 2; i <= 10000000; i++) {
+      // Next Fibonacci number = previous + one before previous
+      // Translated to JavaScript:
+      fib[i] = fib[i - 2] + fib[i - 1];
+    }
+  
+    return fib[index]; 
+  };
+  
+  fibonacci(10000000); 
+  
 
 // Do not edit below this line
 module.exports = fibonacci;
